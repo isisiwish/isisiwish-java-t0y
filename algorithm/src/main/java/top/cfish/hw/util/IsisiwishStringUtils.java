6d9400fa8431aa38dba8de79c1c50bc0893c5be9
isisiwish <isisiwish@gmail.com>
2019-12-07 14:14:55 +0800

    HW

--- algorithm/src/main/java/top/cfish/hw/util/IsisiwishStringUtils.java
+++ algorithm/src/main/java/top/cfish/hw/util/IsisiwishStringUtils.java
@@ -1,5 +1,9 @@
 package top.cfish.hw.util;
 
+import sun.misc.BASE64Decoder;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +13,51 @@ import java.util.regex.Pattern;
  * @time: 21:53
  */
 public class IsisiwishStringUtils {
+
+    // 空字符串
+    public static final String EMPTY_STRING = "";
+
+    // 空String数组
+    public static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+    /**
+     * 检查字符串是否为null或空字符串""
+     */
+    public static boolean isEmpty(String str) {
+        return ((str == null) || (str.length() == 0));
+    }
+
+    /**
+     * 检查字符串是否不为null或空字符串""
+     */
+    public static boolean isNotEmpty(String str) {
+        return ((str != null) && (str.length() > 0));
+    }
+
+    /**
+     * 检查字符串是否是空白、null、空字符串""或只有空白字符
+     * StringUtil.isBlank(null)      = true
+     * StringUtil.isBlank("")        = true
+     * StringUtil.isBlank(" ")       = true
+     * StringUtil.isBlank("bob")     = false
+     * StringUtil.isBlank("  bob  ") = false
+     */
+    public static boolean isBlank(String str) {
+        int length;
+
+        if ((str == null) || ((length = str.length()) == 0)) {
+            return true;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     /**
      * 验证是否为手机号码
      */
@@ -57,6 +106,124 @@ public class IsisiwishStringUtils {
         }
 
         return count;
+    }
+
+    /**
+     * 反转字符串的大小写。
+     */
+    public static String swapCase(String str) {
+        int strLen;
+
+        if ((str == null) || ((strLen = str.length()) == 0)) {
+            return str;
+        }
+
+        StringBuffer buffer = new StringBuffer(strLen);
+
+        char ch = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                ch = Character.toLowerCase(ch);
+            } else if (Character.isTitleCase(ch)) {
+                ch = Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                ch = Character.toUpperCase(ch);
+            }
+
+            buffer.append(ch);
+        }
+
+        return buffer.toString();
+    }
+
+    /**
+     * 将字符串按空白字符分割
+     *
+     * 分隔符不会出现在目标数组中，连续的分隔符被看作一个，如果字符串为null，则返回null
+     *
+     * StringUtil.split(null)       = null
+     * StringUtil.split("")         = []
+     * StringUtil.split("abc def")  = ["abc", "def"]
+     * StringUtil.split("abc  def") = ["abc", "def"]
+     * StringUtil.split(" abc ")    = ["abc"]
+     *
+     * @param str 要分割的字符串
+     * @return 分割后的字符串数组
+     */
+    public static String[] split(String str) {
+        return split(str, ' ');
+    }
+
+    /**
+     * 将字符串按指定字符分割
+     *
+     * 分隔符不会出现在目标数组中，连续的分隔符被看作一个，如果字符串为null，则返回null
+     *
+     * StringUtil.split(null, *)         = null
+     * StringUtil.split("", *)           = []
+     * StringUtil.split("a.b.c", '.')    = ["a", "b", "c"]
+     * StringUtil.split("a..b.c", '.')   = ["a", "b", "c"]
+     * StringUtil.split("a:b:c", '.')    = ["a:b:c"]
+     * StringUtil.split("a b c", ' ')    = ["a", "b", "c"]
+     *
+     * @param str           要分割的字符串
+     * @param separatorChar 分隔符
+     * @return 分割后的字符串数组
+     */
+    public static String[] split(String str, char separatorChar) {
+        if (str == null) {
+            return null;
+        }
+
+        int length = str.length();
+
+        if (length == 0) {
+            return EMPTY_STRING_ARRAY;
+        }
+
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        int start = 0;
+        boolean match = false;
+
+        while (i < length) {
+            if (str.charAt(i) == separatorChar) {
+                if (match) {
+                    list.add(str.substring(start, i));
+                    match = false;
+                }
+
+                start = ++i;
+                continue;
+            }
+
+            match = true;
+            i++;
+        }
+
+        if (match) {
+            list.add(str.substring(start, i));
+        }
+
+        return (String[])list.toArray(new String[list.size()]);
+    }
+
+    public static String Base64Decode(String s) {
+        byte[] b = null;
+        String result = null;
+        if (s != null) {
+            BASE64Decoder decoder = new BASE64Decoder();
+            try {
+                b = decoder.decodeBuffer(s);
+                result = new String(b, "utf-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
